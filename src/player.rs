ce3dff2afd18df2779f02eac68b98076486abd57
Raptor2198 <steeveandriatsilavomitchel@gmail.com>
2024-07-02 17:57:03 +0200

    création de joueur

--- src/player.rs
+++ src/player.rs
@@ -1,54 +1,15 @@
-use crate::map::MapTile;
-
-#[derive(Clone, Copy)]
-pub enum PlayerDirection {
-    North,
-    South,
-    East,
-    West,
+#[derive(Debug)]
+pub struct Player {
+    pub name: String,
+    pub score: u32,
 }
 
-impl PlayerDirection {
-    // Decide of a display direction depending on a movement
-    pub fn from_move(x: isize, y: isize) -> Option<PlayerDirection> {
-        if x < 0 {
-            Some(PlayerDirection::West)
-        } else if y < 0 {
-            Some(PlayerDirection::North)
-        } else if x > 0 {
-            Some(PlayerDirection::East)
-        } else if y > 0 {
-            Some(PlayerDirection::South)
-        } else {
-            None
-        }
-    }
-}
-
-// Struct containing map-related metadata about a player
-pub struct PlayerMapData<T: MapTile> {
-    pub coord: (usize, usize),
-    tile_under: T,
-    pub direction: PlayerDirection,
-}
-
-impl<T: MapTile> PlayerMapData<T> {
-    pub fn init(tile_under: T, coord: (usize, usize)) -> PlayerMapData<T> {
-        PlayerMapData {
-            coord,
-            tile_under,
-            direction: PlayerDirection::North, // Some default direction
-        }
+impl Player {
+    pub fn new(name: String) -> Player {
+        Player { name, score: 0 }
     }
 
-    pub fn replace_tile_under(&mut self, new_tile: T) -> T {
-        std::mem::replace(&mut self.tile_under, new_tile)
+    pub fn increment_score(&mut self) {
+        self.score += 1;
     }
-}
-
-// Trait containing every behavior expected from a Player
-pub trait Player {
-    fn init(id: usize) -> Self
-    where
-        Self: Sized;
 }
